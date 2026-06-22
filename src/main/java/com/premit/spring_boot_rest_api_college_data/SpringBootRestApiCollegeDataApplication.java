9c39dffb8965cd76e4744185a88ed115bd12891f
premsai7744 <premsai7744@gmail.com>
2026-06-22 10:22:56 +0530

    Initial commit.

---
+++ src/main/java/com/premit/spring_boot_rest_api_college_data/SpringBootRestApiCollegeDataApplication.java
@@ -0,0 +1,13 @@
+package com.premit.spring_boot_rest_api_college_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiCollegeDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiCollegeDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/premit/spring_boot_rest_api_college_data/SpringBootRestApiCollegeDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premit.spring_boot_rest_api_college_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiCollegeDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
